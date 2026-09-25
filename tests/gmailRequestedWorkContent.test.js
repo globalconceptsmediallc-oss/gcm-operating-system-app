1c082bbba9edfc80f040484dc866ea3710aa3bbf
globalconceptsmediallc-oss <globalconceptsmediallc@gmail.com>
2026-09-25 15:58:09 -0400

    Align Gmail requested work routing version assertions

--- tests/gmailRequestedWorkContent.test.js
+++ tests/gmailRequestedWorkContent.test.js
@@ -18,8 +18,8 @@ const dispositions = read("routes/gmailDispositions.js");
 const operationalDecision = read("routes/operationalDecision.js");
 const migration = read("migrations/0015_gmail_requested_work_content_repair.sql");
 
-assert.match(dispositions, /Version: 2\.2\.2/);
-assert.match(dispositions, /GMAIL_HUMAN_ROUTING_VERSION = "2\.2\.2"/);
+assert.match(dispositions, /Version: 2\.2\.3/);
+assert.match(dispositions, /GMAIL_HUMAN_ROUTING_VERSION = "2\.2\.3"/);
 assert.match(dispositions, /workTitle:disposition === "requested_work"/);
 assert.match(dispositions, /workDescription:requestedWorkSource/);
 assert.match(dispositions, /sanitizeEmailText\(message\.bodyText \|\| message\.snippet \|\| message\.subject\)/);
